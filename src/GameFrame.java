aa6318e8cbdc1df0a1211b1fa16afa107a14c8d2
RENOLEVES <zzhao260985652@gmail.com>
2021-07-27 17:20:03 -0400

    创建窗口

---
+++ src/GameFrame.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+
+public class GameFrame extends Frame {
+
+    static final int GAME_WIDTH = 600, GAME_HEIGHT= 600;
+    public GameFrame(){
+        setSize(GAME_WIDTH,GAME_HEIGHT);
+        setResizable(false);
+        setTitle("Game 2048 Recovered");
+        setVisible(true);
+    }
+
+
+
+
+}
